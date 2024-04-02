edc75dd9406ec103a078601dde6250f14bd2f417
Cesar A Camargo <131175982+CesarACamargo@users.noreply.github.com>
2024-04-01 22:31:48 -0300

    Delete TF-Auto-Scale-Group directory

--- TF-Auto-Scale-Group/ASG+LT/output.tf
+++
@@ -1,7 +0,0 @@
-output "aws_launch_template" {
-  value = aws_launch_template.ltlab.id
-}
-
-output "aws_autoscaling_group" {
-  value = aws_autoscaling_group.asglab.name
-}
--- TF-Auto-Scale-Group/ASG/asg.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_autoscaling_group" "asglab" {
-    name = "asg-lab"
-    desired_capacity = 1
-    max_size = 1
-    min_size = 1
-}
-
