c2a84698a3cb7e120bb6fe5fba4fa81b860c74a6
Cesar A Camargo <cesar@leap-15.example.com>
2024-04-01 23:13:53 -0300

    Foi retirado os comentarios das linhas

--- ot-ssm-vpc-endpoint-parte-1/main.tf
+++ ot-ssm-vpc-endpoint-parte-1/main.tf
@@ -12,7 +12,7 @@ module "security_resources" {
   vpc_id                  = module.network-vpc.vpc_ssm_id
   private_subnet_ssm_cidr = module.network-vpc.private_subnet_ssm_cidr
 }
-/*
+
 ## Instances EC2
 module "ec2-bastion" {
   source = "./modules/ec2-bastion"
@@ -20,8 +20,3 @@ module "ec2-bastion" {
   private_sg_id = module.security_resources.access_bastion_sg
   private_subnet_id = module.network-vpc.private_subnet_ssm_id
 }
-
-/*
-# Acessar instancia via ssm
-# aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartInteractiveCommand --parameters command="bash -l"
-*/
